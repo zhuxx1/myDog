4a70a0d52da8b48070cc2ae7acd7ebf49c27ab80
zhuxingxing <zhuxingxing@ruqimobility.com>
2022-07-17 16:18:46 +0800

    添加my_dog代码

---
+++ isaacgym-Master/python/examples/my_dog.py
@@ -0,0 +1,126 @@
+from isaacgym import gymapi, gymutil
+
+asset_descriptors = [
+    AssetDesc("urdf/CatModel2/urdf/CatModel2.urdf", False),
+]
+
+# parse arguments
+args = gymutil.parse_arguments(
+    description="My dog: Animate degree-of-freedom ranges",
+    custom_parameters=[
+        {"name": "--asset_id", "type": int, "default": 0, "help": "Asset id (0 - %d)" % (len(asset_descriptors) - 1)},
+        {"name": "--speed_scale", "type": float, "default": 1.0, "help": "Animation speed scale"},
+        {"name": "--show_axis", "action": "store_true", "help": "Visualize DOF axis"}])
+
+# initialize gym
+gym = gymapi.acquire_gym()
+
+# configure sim
+sim_params = gymapi.SimParams()
+sim_params.dt = dt = 1.0 / 60.0
+
+if args.physics_engine == gymapi.SIM_FLEX:
+    pass
+elif args.physics_engine == gymapi.SIM_PHYSX:
+    sim_params.physx.solver_type = 1
+    sim_params.physx.num_position_iterations = 6
+    sim_params.physx.num_velocity_iterations = 0
+    sim_params.physx.num_threads = args.num_threads
+    sim_params.physx.use_gpu = args.use_gpu
+
+sim_params.use_gpu_pipeline = False
+if args.use_gpu_pipeline:
+    print("WARNING: Forcing CPU pipeline.")
+
+sim = gym.create_sim(args.compute_device_id, args.graphics_device_id, args.physics_engine, sim_params)
+if sim is None:
+    print("*** Failed to create sim")
+    quit()
+
+# add ground plane
+plane_params = gymapi.PlaneParams()
+gym.add_ground(sim, plane_params)
+
+# create viewer
+viewer = gym.create_viewer(sim, gymapi.CameraProperties())
+if viewer is None:
+    print("*** Failed to create viewer")
+    quit()
+
+# load asset
+asset_root = "../../assets"
+asset_file = asset_descriptors[args.asset_id].file_name
+
+asset_options = gymapi.AssetOptions()
+asset_options.fix_base_link = True
+asset_options.flip_visual_attachments = asset_descriptors[args.asset_id].flip_visual_attachments
+asset_options.use_mesh_materials = True
+
+print("Loading asset '%s' from '%s'" % (asset_file, asset_root))
+asset = gym.load_asset(sim, asset_root, asset_file, asset_options)
+
+# set up the env grid
+num_envs = 36
+num_per_row = 6
+spacing = 2.5
+env_lower = gymapi.Vec3(-spacing, 0.0, -spacing)
+env_upper = gymapi.Vec3(spacing, spacing, spacing)
+
+# position the camera
+cam_pos = gymapi.Vec3(17.2, 2.0, 16)
+cam_target = gymapi.Vec3(5, -2.5, 13)
+gym.viewer_camera_look_at(viewer, None, cam_pos, cam_target)
+
+# cache useful handles
+envs = []
+actor_handles = []
+
+print("Creating %d environments" % num_envs)
+for i in range(num_envs):
+    # create env
+    env = gym.create_env(sim, env_lower, env_upper, num_per_row)
+    envs.append(env)
+
+    # add actor
+    pose = gymapi.Transform()
+    pose.p = gymapi.Vec3(0.0, 1.32, 0.0)
+    pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
+
+    actor_handle = gym.create_actor(env, asset, pose, "actor", i, 1)
+    actor_handles.append(actor_handle)
+
+    # set default DOF positions
+    gym.set_actor_dof_states(env, actor_handle, dof_states, gymapi.STATE_ALL)
+
+# joint animation states
+ANIM_SEEK_LOWER = 1
+ANIM_SEEK_UPPER = 2
+ANIM_SEEK_DEFAULT = 3
+ANIM_FINISHED = 4
+
+# initialize animation state
+anim_state = ANIM_SEEK_LOWER
+current_dof = 0
+print("Animating DOF %d ('%s')" % (current_dof, dof_names[current_dof]))
+
+while not gym.query_viewer_has_closed(viewer):
+
+    # step the physics
+    gym.simulate(sim)
+    gym.fetch_results(sim, True)
+
+    speed = speeds[current_dof]
+
+# update the viewer
+    gym.step_graphics(sim)
+    gym.draw_viewer(viewer, sim, True)
+
+    # Wait for dt to elapse in real time.
+    # This synchronizes the physics simulation with the rendering rate.
+    gym.sync_frame_time(sim)
+
+print("Done")
+
+gym.destroy_viewer(viewer)
+gym.destroy_sim(sim)
+
